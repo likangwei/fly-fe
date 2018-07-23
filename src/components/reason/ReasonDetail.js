bec80601d91d282d8e5ea2b3dc90486b7f1d599e
李康炜 <likangwei@meicai.cn>
2018-07-23 22:53:22 +0800

    fix ui

--- src/components/reason/ReasonDetail.js
+++ src/components/reason/ReasonDetail.js
@@ -1,17 +1,116 @@
 import React from 'react';
 import { connect } from 'dva';
-import { Table, Pagination, Popconfirm, Button } from 'antd';
+import { 
+  Input,
+  Table,
+  Pagination, 
+  Popconfirm, 
+  Button,
+  Form,
+} from 'antd';
 import { routerRedux } from 'dva/router';
+import FiveWhy from './FiveWhy'
+const FormItem = Form.Item;
 
+
+const { TextArea } = Input;
 const PAGE_SIZE = 1000
 
-export default class ReasonDetail extends React.Component{
+const Why = () => {
+  return (
+    <FormItem
+      id="control-mention"
+      label="为什么"
+      labelCol={{ span: 6 }}
+      wrapperCol={{ span: 16 }}
+    >
+      <Input value={this.props.val}/> 
+    </FormItem>
+  )
+}
+
+const Answser = (val) => {
+  return (
+    <FormItem
+      id="control-mention"
+      label="因为"
+      labelCol={{ span: 6 }}
+      wrapperCol={{ span: 16 }}
+    >
+      <Input value={this.props.val}/> 
+    </FormItem>
+  )
+}
+
+class ReasonDetail extends React.Component{
+
+  constructor(props){
+    super(props)
+    this.state = {
+      analysis: [""]
+    }
+  }
+
+  addRecord = () => {
+    let analysis = this.state.analysis
+    analysis.push("")
+    this.setState({analysis})
+  }
 
   render(){
-    console.log("reason detail")
+    const {list} = this.props
+    let match = this.props.match
+    console.log(match.params.id)
+    let record = null
+    for(let i=0; i<list.length; i++){
+      let cell = list[i]
+      if (cell.id == match.params.id){
+        record = cell
+        break
+      }
+    }
+    if (record == null){
+      return <div>无效的ID</div>
+    }
+    let analysis = this.state.analysis
+    let analysisComp = []
+    for (let i=0; i<analysis.length; i=i+1){
+      let val = analysis[i]
+      if (i%2 == 0){
+        analysisComp.push(<Why val={val}/>)
+      }else{
+        analysisComp.push(<Answser val={val}/>)
+      }
+    }
     return (
-      <div>reason detail</div>
+      <Form layout="horizontal">
+        reason detail
+        <FiveWhy />
+        <TextArea 
+          placeholder="" 
+          autosize={{ minRows: 2, maxRows: 600 }} 
+        />
+        {analysisComp}
+        <Button type="primary" htmlType="submit" onClick={this.addRecord}>
+            添加
+        </Button>
+        <Button type="primary" htmlType="submit">
+            保存
+        </Button>
+      </Form>
     )
   }
 }
 
+
+function mapStateToProps(state) {
+  const { list, total, page } = state.reasons;
+  return {
+    loading: state.loading.models.reasons,
+    list,
+    total,
+    page,
+  };
+}
+
+export default connect(mapStateToProps)(ReasonDetail);
